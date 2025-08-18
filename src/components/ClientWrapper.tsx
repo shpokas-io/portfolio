8810977d865a0030bf6517d27843723d153aa763
Skirmantas Spakovskis <skirmantas.spakovskis@ext.bdo.global>
2025-08-18 18:18:24 +0300

    remove initial loading animation

--- src/components/ClientWrapper.tsx
+++ src/components/ClientWrapper.tsx
@@ -1,25 +1,9 @@
 "use client";
 
-import { useState } from "react";
-import { LoadingScreen } from "./LoadingScreen";
-
 interface ClientWrapperProps {
   children: React.ReactNode;
 }
 
 export function ClientWrapper({ children }: ClientWrapperProps) {
-  const [isLoading, setIsLoading] = useState(true);
-
-  const handleLoadingComplete = () => {
-    setIsLoading(false);
-  };
-
-  return (
-    <>
-      {isLoading && <LoadingScreen onComplete={handleLoadingComplete} />}
-      <div className={isLoading ? "hidden" : "block"}>
-        {children}
-      </div>
-    </>
-  );
+  return <>{children}</>;
 }
